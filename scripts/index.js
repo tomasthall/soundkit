54e4f6108b6834f49630f73a7ef28bedc44fa3cc
tomasthall <gebol1@onet.eu>
2019-01-14 23:38:02 +0100

    feat(app): add working recording feature

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,88 @@
+const channel1 = [];
+let recStart = 0;
+let isRecording = false;
 
+let audioDOM = document.querySelector('#audioDOMe');
+let audioName = '';
 let soundboard = document.querySelector('.soundboard');
 let soundBoxes = [];
+let sounds = [
+    {key: '110', src: 'sounds/hats2.wav', name: 'hat'},
+    {key: '98', src: 'sounds/kick2.wav', name: 'kick'},
+    {key: '99', src: 'sounds/snare2.mp3', name: 'snare'}
+    ];
 
-let numberOfBoxes = 12;
-for (let i = 0; i < numberOfBoxes; i++) {
-    let box = document.createElement('div');
-    box.className = 'soundboard__box';
-    box.innerHTML = `box${i}`;
-    soundboard.appendChild(box);
-    soundBoxes.push(box);
+let numberOfBoxes = 3;
+
+function recordMusic(e) {
+    recStart = Date.now();
+    isRecording = !isRecording;
+    if(isRecording) {
+        document.querySelector('#rec').style.color = 'pink';
+    } else {
+        document.querySelector('#rec').style.color = 'white';
+    }
+}
+document.addEventListener('DOMContentLoaded', appStart);
+
+function appStart() {
+    document.querySelector('#rec').addEventListener('click', recordMusic)
+    for (let i = 0; i < numberOfBoxes; i++) {
+        let box = document.createElement('div');
+        box.className = 'soundboard__box';
+        box.innerHTML = sounds[i].name;
+        let audio = document.createElement('audio');
+        audio.id = 'audio' + sounds[i].key;
+        audio.style.display = 'none';
+        let source = document.createElement('source');
+        source.src = sounds[i].src;
+        source.type = 'audio/mpeg';
+        audio.appendChild(source);
+        box.appendChild(audio);
+        box.addEventListener('click', (e) => {
+            document.getElementById('audio' + i).play();
+        });
+        soundboard.appendChild(box);
+        soundBoxes.push(box);
+    }
+    window.addEventListener('keypress', playSound);
+    document.querySelector('#play').addEventListener('click', playMusic);
+    document.querySelector('#rec').addEventListener('click', recordMusic);
 }
 
-console.log(soundBoxes);
+function playSound(e) {
+    let key = 0;
+    audioName = sounds.filter( sound => {
+        if(sound.key == e.charCode) {
+            key = sound.key;
+            return sound.key;
+        }
+    });
+    audioDOM = document.querySelector(`#audio${key}`);
+    audioDOM.currentTime = 0;
+    audioDOM.play();
+
+    if(isRecording) {
+        let timer = Date.now() - recStart;
+        channel1.push(
+            {
+                name: key,
+                time: timer
+            }
+        );
+    }
+}
+
+function playMusic() {
+    channel1.forEach(sound => {
+        setTimeout(
+            () => {
+                console.log(sound);
+                audioDOM = document.querySelector(`#audio${sound.name}`);
+                audioDOM.currentTime = 0;
+                audioDOM.play()
+            }
+            , sound.time
+        )
+    })
+}
